e47a803a87aeab926036ec9de7dffd2c1f984ff3
geragora <103779549+geragora@users.noreply.github.com>
2024-02-28 14:29:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ import yfinance as yf
 import plotly.express as px
 from plotly.subplots import make_subplots
 import psycopg2
-# from datetime import  timedelta
 import datetime
-import streamlit as st
-import plotly.graph_objects as go
-import numpy as np
 
 st.write('treck')
